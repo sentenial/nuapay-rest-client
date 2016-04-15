324892c9261166c2a7a0668468ef2c3cc2933cf8
Michal Balicki <michal.balicki@amartus.com>
2016-04-15 12:21:15 +0200

    Added statusCode property in SentenialException allowing retrieving http status code.

--- src/main/java/com/sentenial/rest/client/api/common/service/SentenialException.java
+++ src/main/java/com/sentenial/rest/client/api/common/service/SentenialException.java
@@ -9,6 +9,8 @@ public class SentenialException extends RuntimeException {
 	private static final long serialVersionUID = -9018466630393618186L;
 	
 	private ErrorResponse errorResponse;
+	
+	private Integer statusCode;
 
 	public SentenialException(String msg) {
 		super(msg);
@@ -19,16 +21,22 @@ public class SentenialException extends RuntimeException {
 	}
 
 	public SentenialException(String url, int statusCode, String responseString) {    
-		super(format("[url: %s, error code: %d, response: %s]", url, statusCode, responseString));    
+		super(format("[url: %s, error code: %d, response: %s]", url, statusCode, responseString)); 
+		this.statusCode = statusCode;
 	}
 	
 	public SentenialException(String url, int statusCode, ErrorResponse errorResponse) {    
 		super(format("[url: %s, error code: %d, response: %s]", url, statusCode, errorResponse));
+		this.statusCode = statusCode;
 		this.errorResponse = errorResponse;
 	}
 
 	public ErrorResponse getErrorResponse() {
 		return errorResponse;
+	}
+
+	public Integer getStatusCode() {
+		return statusCode;
 	}
 	
 }
