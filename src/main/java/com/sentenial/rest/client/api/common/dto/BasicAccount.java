483c568c777b1dd140257833bb5a5d8886f3e75f
Michal Balicki <michal.balicki@amartus.com>
2016-09-13 12:32:53 +0200

    New fields in BasicAccount describing domestic account

--- src/main/java/com/sentenial/rest/client/api/common/dto/BasicAccount.java
+++ src/main/java/com/sentenial/rest/client/api/common/dto/BasicAccount.java
@@ -6,6 +6,12 @@ public class BasicAccount {
 	private String iban;
 	
 	private String bic;
+	
+	private String domesticAccountNumber;
+	
+	private String domesticBranchCode;
+	
+	private String accountCountry;
 
 	
 	public BasicAccount withIban(String iban) {
@@ -15,6 +21,21 @@ public class BasicAccount {
 	
 	public BasicAccount withBic(String bic) {
 		this.bic = bic;
+		return this;
+	}
+	
+	public BasicAccount withDomesticAccountNumber(String domesticAccountNumber) {
+		this.domesticAccountNumber = domesticAccountNumber;
+		return this;
+	}
+
+	public BasicAccount withDomesticBranchCode(String domesticBranchCode) {
+		this.domesticBranchCode = domesticBranchCode;
+		return this;
+	}
+
+	public BasicAccount withAccountCountry(String accountCountry) {
+		this.accountCountry = accountCountry;
 		return this;
 	}
 	
@@ -34,9 +55,34 @@ public class BasicAccount {
 		this.bic = bic;
 	}
 
+	public String getDomesticAccountNumber() {
+		return domesticAccountNumber;
+	}
+
+	public void setDomesticAccountNumber(String domesticAccountNumber) {
+		this.domesticAccountNumber = domesticAccountNumber;
+	}
+
+	public String getDomesticBranchCode() {
+		return domesticBranchCode;
+	}
+
+	public void setDomesticBranchCode(String domesticBranchCode) {
+		this.domesticBranchCode = domesticBranchCode;
+	}
+
+	public String getAccountCountry() {
+		return accountCountry;
+	}
+
+	public void setAccountCountry(String accountCountry) {
+		this.accountCountry = accountCountry;
+	}
+
 	@Override
 	public String toString() {
-		return "DebtorAccount [iban=" + iban + ", bic=" + bic + "]";
+		return "BasicAccount [iban=" + iban + ", bic=" + bic + ", domesticAccountNumber=" + domesticAccountNumber
+				+ ", domesticBranchCode=" + domesticBranchCode + ", accountCountry=" + accountCountry + "]";
 	}
 
 }
